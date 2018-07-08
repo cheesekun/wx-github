aa3716232f911aed4e261879626d3a04afc43b58
cheesekun <cheesekun1997@gmail.com>
2018-07-08 20:49:18 +0800

    update loading component

--- src/components/loading/loading.vue
+++ src/components/loading/loading.vue
@@ -1,6 +1,10 @@
 <template>
   <div class="loading">
-    <span class="icon-spinner rotate-icon"></span> 加载中
+    <div class="rect1"></div>
+    <div class="rect2"></div>
+    <div class="rect3"></div>
+    <div class="rect4"></div>
+    <div class="rect5"></div>
   </div>
 </template>
 
