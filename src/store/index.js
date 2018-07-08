ab2c1cea2c2d102ec09d264fd56b2c3161a3ba2d
cheesekun <cheesekun1997@gmail.com>
2018-07-08 12:34:18 +0800

    add vuex.state topH

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,3 @@
-// https://vuex.vuejs.org/zh-cn/intro.html
-// make sure to call Vue.use(Vuex) if using a module system
 import Vue from 'vue'
 import Vuex from 'vuex'
 
@@ -7,19 +5,12 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
   state: {
-    count: 0,
     login: '',
-    repo: ''
-
+    tabsH: ''
   },
   mutations: {
-    increment: (state) => {
-      const obj = state
-      obj.count += 1
-    },
-    decrement: (state) => {
-      const obj = state
-      obj.count -= 1
+    assignTabsH (state, height) {
+      state.tabsH = height
     }
   }
 })
