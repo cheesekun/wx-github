b5c48a9b80dbb76c96597e7080b8b6100dfe28e5
cheesekun <cheesekun1997@gmail.com>
2018-07-15 00:32:26 +0800

    add nodata/loadend components

---
+++ src/components/loadEnd/loadEnd.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>____没啦_____</div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped lang='scss'>
+  @import './loadEnd.scss';
+</style>
---
+++ src/components/noData/noData.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>____no data____</div>
+</template>
+
+<script>
+  export default {
+
+  }
+</script>
+
+<style scoped lang='scss'>
+  @import './noData.scss';
+</style>
