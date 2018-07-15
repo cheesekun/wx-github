5424fbfe0c4ddbc6f705cb5ca781a4f218ba686e
cheesekun <cheesekun1997@gmail.com>
2018-07-16 01:33:31 +0800

    update info page

--- src/pages/info/info.vue
+++ src/pages/info/info.vue
@@ -7,27 +7,18 @@
 <script>
 import api from '@/utils/api'
 import User from '@/components/user/user'
+import { getQuery } from '@/utils'
 // import wx from 'wx'
 
 export default {
   /**
    * FIXME: 在点击后退键时，路由问题，mpvue大bug卧槽
-   * TODO: 把下面的方法封装一下
    * FIXME: 还是有bug，感觉还是数据清理的问题
    */
-  // async mounted () {
   async onShow () {
-    /* eslint-disable */
-    var pages = getCurrentPages()
-    var currentPage = pages[pages.length - 1]
-    var url = currentPage.route
-    var options = currentPage.options
-    console.log(options)
-    // console.log(this.$root.$mp.appOptions)
-    // console.log(this.$root.$mp.query)
     this.info = {}
-    // let user = this.$root.$mp.query.login
-    let user = options.login
+    const options = getQuery()
+    const user = options.login
     this.info = await this.getInfo(user)
   },
   /**
@@ -36,12 +27,6 @@ export default {
    */
   onHide () {
     this.info = {}
-  },
-  onUnload () {
-    console.log(4)
-    // this.$root.$mp.query = {}
-    // console.log(this.$root.$mp.appOptions)
-    // console.log(this.$root.$mp.query)
   },
   data () {
     return {
