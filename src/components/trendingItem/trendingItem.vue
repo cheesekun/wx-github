5d9660e060463abc97767528bccfe1e7f2f52148
cheesekun <cheesekun1997@gmail.com>
2018-07-16 01:05:32 +0800

    update trending page

---
+++ src/components/trendingItem/trendingItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <div @click="toRepo(trending.owner.login, trending.name)" class="trending-item">
+    <div class="top">
+      <p class="trending-name">{{trending.owner.login}}/{{trending.name}}</p>
+      <p class="trending-lang" v-if="trending.language">{{trending.language}}<span class="lang-color" :style="{background: trending.color}"></span></p>
+    </div>
+    <p class="trending-desc">{{trending.description}}</p>
+    <div class="bottom">
+      <p class="icon-star"> {{trending['stargazers_count']}}</p>
+      <p class="icon-flow-branch"> {{trending['forks_count']}}</p>
+      <p class="icon-star increment"> {{trending.increment}} star today</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      trending: {
+        types: Object,
+        defalt: {}
+      }
+    },
+    data () {
+      return {}
+    },
+    methods: {
+      toInfo (user) {
+        wx.navigateTo({
+          url: `/pages/info/info?login=${user}`
+        })
+      },
+      toRepo (user, repo) {
+        wx.navigateTo({
+          url: `/pages/repo/repo?owner=${user}&repo=${repo}`
+        })
+      }
+    }
+  }
+</script>
+
+<style scoped lang='scss'>
+  @import './trendItem.scss';
+</style>
--- src/pages/trending/trending.vue
+++ src/pages/trending/trending.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="trending-container">
-    <div class="picker">
+    <div id="picker" class="picker">
       <div class="bg"></div>
       <div class="picker-item">
         <picker @change="pickerChange" :value="index" :range="langs">
@@ -13,35 +13,63 @@
     <div id="tabs" class="tabs">
       <Tabs @getTab="getTab" :tabs="tabs" :index="currentIndex" />
     </div>
+    <!-- <Loading /> -->
+    <swiper @change="pageChange" :style="{height: height}" class="list" :current-item-id="currentId" duration="200">
+      <swiper-item item-id="daily">
+        <scroll-view
+        class="list-view"
+        :style="{height: height}"
+        enable-back-to-top="true"
+        scroll-y="true">
+          <div class="repo-item" v-for="(item, index) in trendingD" :key="index">
+            <trending-item :trending="item"></trending-item>
+          </div>
+        </scroll-view>
+      </swiper-item>
+      <swiper-item item-id="weekly">
+        <p>2</p>
+      </swiper-item>
+      <swiper-item item-id="monthly">
+        <p>3</p>
+      </swiper-item>
+    </swiper>
   </div>
 </template>
 
 <script>
 /**
  * FIXME: 那个高1px是什么玩意
+ * TODO: 给weekly和monthly添加转场的loading
 */
 // import wx from 'wx'
 // import api from '@/utils/api'
 import Tabs from '@/components/tabs/tabs'
+import Loading from '@/components/loading/loading'
+import TrendingItem from '@/components/trendingItem/trendingItem'
+import api from '@/utils/api'
+import { dealTrending } from '@/utils'
 import { mapMutations } from 'vuex'
 
 export default {
+  async created () {
+    wx.getSystemInfo({
+      success: (res) => {
+        this.height = res.windowHeight
+      }
+    })
+
+    this.trendingD = await this.getTrending(this.currentId, this.langs[this.index])
+  },
   onLoad () {
-    // wx.getSystemInfo({
-    //   success: (res) => {
-    //     this.height = res.windowHeight
-    //   }
-    // })
     let query = wx.createSelectorQuery()
-    // // 选择id
-    // query.select('#search').boundingClientRect()
+
+    query.select('#picker').boundingClientRect()
     query.select('#tabs').boundingClientRect()
     query.exec(res => {
-      // let searchH = res[0].heihgt
-      let tabsH = res[0].height
-      // this.height = this.height - searchH - tabsH
-      // console.log(res)
-      console.log(tabsH)
+      let pickerH = res[0].height
+      let tabsH = res[1].height
+
+      this.height = this.height - pickerH - tabsH + 'px'
       this.assignTabsH(tabsH)
     })
   },
@@ -59,11 +87,18 @@ export default {
         id: 'monthly',
         name: 'MONTHLY'
       }],
-      currentIndex: 1
+      height: '',
+      trendingD: [],
+      trendingW: [],
+      trendingM: [],
+      currentId: 'daily',
+      currentIndex: 0
     }
   },
   components: {
-    Tabs
+    Tabs,
+    Loading,
+    TrendingItem
   },
   methods: {
     pickerChange (e) {
@@ -73,6 +108,10 @@ export default {
     getTab (data) {
       // this.currentId = data.id
       console.log(data)
+    },
+    async getTrending (time, lang) {
+      const data = await api.getTrending(time, lang)
+      return dealTrending(data.data)
     },
     ...mapMutations([
       'assignTabsH'
