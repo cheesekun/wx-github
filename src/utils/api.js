35a4f1fa141369bb77afb9305cd094cc4628c1ee
cheesekun <cheesekun1997@gmail.com>
2018-07-20 00:42:07 +0800

    add follow/star api

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,103 +1,102 @@
-
 import request from './request'
 
-// const baseUrl = 'https://api.github.com'
-const baseUrl = ''
-const headers = {
-  'x-id': 0,
-  'Authorization': ''
-}
+// const baseURL = 'https://api.github.com'
+const baseURL = 'https://github.konsonx.com'
+// const baseURL = ''
 
 const api = {
   getUsers: (q) => request.get('/search/users', {...q}, {
-    baseURL,
-    headers
+    baseURL
   }),
   getRepos: (q) => request.get('/search/repositories', {...q}, {
-    baseURL,
-    headers
+    baseURL
   }),
   getCode: (q) => request.get('/search/code', {...q}, {
-    baseURL,
-    headers
+    baseURL
   }),
   getIssues: (q) => request.get('/search/issues', {...q}, {
-    baseURL,
-    headers
+    baseURL
   }),
 
-  /**
-   * me,自己的账户
-   * FIXME: 记得改
-   */
-  // getMe: () => request.get('/user', null, {
-  //   baseURL: ,
-  //   headers
-  // }),
-  getMe: () => request.get(`/users/cheesekun`, null, {
-    baseURL,
-    headers
+  getMe: () => request.get(`/user`, null, {
+    baseURL
   }),
 
   login: (username, password) => request.post('/login', {username, password}, {
-    baseURL:
+    baseURL
   }),
 
   getInfo: (user) => request.get(`/users/${user}`, null, {
-    baseURL,
-    headers
+    baseURL
   }),
 
   getRepo: (owner, repo) => request.get(`/repos/${owner}/${repo}`, null, {
-    baseURL,
-    headers
+    baseURL
   }),
 
   getOwnRepos: (user) => request.get(`/users/${user}/repos`, null, {
-    baseURL,
-    headers
+    baseURL
   }),
 
   /**
    * FIXME: 改下文档
    */
   getStarred: (user, q) => request.get(`/users/${user}/starred`, q, {
-    baseURL,
-    headers
+    baseURL
   }),
 
   getEvents: (user) => request.get(`/users/${user}/events`, null, {
-    baseURL,
-    headers
+    baseURL
   }),
 
   getFollowers: (user) => request.get(`/users/${user}/followers`, null, {
-    baseURL,
-    headers
+    baseURL
   }),
 
   getFollowing: (user) => request.get(`/users/${user}/following`, null, {
-    baseURL,
-    headers
+    baseURL
   }),
 
   getReadme: (owner, repo) => request.get(`/repos/${owner}/${repo}/contents/README.md`, null, {
-    baseURL: ,
-    headers
+    baseURL
   }),
 
   getCommits: (owner, repo, q) => request.get(`/repos/${owner}/${repo}/commits`, q, {
   // getCommits: (owner, repo) => request.get(`/repos/${owner}/${repo}/commits`, null, {
-    baseURL: ,
-    headers
+    baseURL
   }),
 
   /**
-   * TODO: 对baseUrl做处理
+   * TODO: 对baseURL做处理
    * TODO: 之后统一对code做处理
   */
   getTrending: (time, lang) => request.get(`/trending`, {time, lang}, {
-    baseURL:
+    baseURL
+  }),
+
+  /**
+   * 判断用户follow
+  */
+  getIsFollow: (user) => request.get(`/user/following/${user}`, null, {
+    baseURL
+  }),
+  putFollow: (user) => request.put(`/user/following/${user}`, null, {
+    baseURL
+  }),
+  deleteFollow: (user) => request.delete(`/user/following/${user}`, null, {
+    baseURL
+  }),
+  /**
+   * 判断仓库star
+  */
+  getIsStar: (owner, repo) => request.get(`/user/starred/${owner}/${repo}`, null, {
+    baseURL
+  }),
+  putStar: (owner, repo) => request.put(`/user/starred/${owner}/${repo}`, null, {
+    baseURL
+  }),
+  deleteStar: (owner, repo) => request.delete(`user/starred/${owner}/${repo}`, null, {
+    baseURL
   })
 }
 
