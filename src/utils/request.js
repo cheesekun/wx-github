1cd665b8f2a87dd290374e3acd3e5c55dadd30ee
cheesekun <cheesekun1997@gmail.com>
2018-07-20 00:41:17 +0800

    add default headers

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,10 +1,28 @@
 import wx from 'wx'
 import Fly from 'flyio'
-// import { ERR, OK } from '@/utils/config'
+import { REQ_ERR, REQ_OK, OAUTH_ERR } from '@/utils/config'
 
 const request = new Fly()
 
+function getAuth () {
+  let headers = {}
+  let auth = wx.getStorageSync('auth')
+  if (auth === '') {
+    headers = {
+      'x-id': 0,
+      'Authorization': ''
+    }
+  } else {
+    let authObj = JSON.parse(auth)
+    headers['x-id'] = authObj['oauth_id']
+    headers['Authorization'] = authObj['hash_token']
+  }
+  return headers
+}
+
 request.interceptors.request.use((request) => {
+  request.headers = getAuth()
+  console.log(getAuth())
   wx.showNavigationBarLoading()
   return request
 })
@@ -12,14 +30,23 @@ request.interceptors.request.use((request) => {
 request.interceptors.response.use(
   (response, promise) => {
     wx.hideNavigationBarLoading()
-    // if (response.data.code !== OK) {
-    //   wx.showToast({
-    //     title: '好像gg了.',
-    //     icon: 'success',
-    //     duration: 1500
-    //   })
-    // }
-    return promise.resolve(response.data)
+    const code = response.data.code
+    if (code === REQ_ERR) {
+      wx.showToast({
+        title: '我猜github挂了',
+        icon: 'success',
+        duration: 1500
+      })
+    } else if (code === REQ_OK) {
+      return promise.resolve(response.data.data)
+    } else if (code === OAUTH_ERR) {
+      wx.showToast({
+        title: '我猜账户密码输入有误',
+        icon: 'success',
+        duration: 2000
+      })
+      return promise.resolve(response.data.data)
+    }
   },
   (err, promise) => {
     wx.hideNavigationBarLoading()
